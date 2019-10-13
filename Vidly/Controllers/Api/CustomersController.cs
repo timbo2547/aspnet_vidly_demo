0510f84ec843e1f08a4ae50735956adab4aaf926
timbo2547 <timbo2547@gmail.com>
2019-10-13 11:53:41 -0500

    API and Mapping to DTOs

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.Dtos;
+using AutoMapper;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET /api/customers
+        public IEnumerable<CustomerDto> GetCustomers()
+        {         
+            //Maps Customer to CustomerDto with delegate method reference
+            return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+        }
+
+        // GET /api/customers/1
+        public CustomerDto GetCustommer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(x => x.Id == id);
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            //Maps Customer to new CustomerDto and returns it
+            return Mapper.Map<Customer, CustomerDto>(customer);
+        }
+
+        // POST /api/customers
+        [HttpPost]//Only called if sending HttpPost request. Used for Adding
+        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            //Map customerDto to new customer 
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            //Customer added to Database creates Id
+            customerDto.Id = customer.Id;
+
+            return customerDto;
+        }
+
+        // PUT /api/customers/1
+        [HttpPut]//Only called if sending HttpPut request. Used for updating
+        public void UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            var customerInDb = _context.Customers.SingleOrDefault(x => x.Id == id);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            //Map customerDto to customerInDb 
+            Mapper.Map(customerDto, customerInDb);
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]//Only called if sending HttpDelete request. Used for deleting
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(x => x.Id == id);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
+
+    }
+}
